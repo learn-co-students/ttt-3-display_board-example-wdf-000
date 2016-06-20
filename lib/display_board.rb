323cb55d415427b3ce7fa2417eec86d1cb5937ab
Gordon Gao <gordonsdev@gmail.com>
2016-06-20 16:01:23 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  board = Array.new(9) {"   "}
+  separator = "-----------"
+
+  while board.count > 0
+    arr = board.take(3)
+    puts "#{arr[0]}|#{arr[1]}|#{arr[2]}"
+    puts separator if board.count > 3
+    board.shift(3)
+  end
+end
