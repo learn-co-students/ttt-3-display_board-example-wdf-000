31eba42fa424fc1cea233e74960e36b0ed738e29
alexgriff <alexalexgriffith@gmail.com>
2016-06-21 20:54:58 -0400

    Finito

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,16 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  print_row
+  print_divider
+  print_row
+  print_divider
+  print_row
+end
+
+def print_row
+  puts "   |   |   "
+end
+
+def print_divider
+  puts (1..11).map { "-" }.join
+end
